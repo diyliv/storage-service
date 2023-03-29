041515fe8fa49db1df7459cb4eb8dc1257069835
diyliv <onlogn081@gmail.com>
2023-03-29 18:01:54 +0300

    main file changes: initialize connection with timescaledb, initialize config, defer func to close connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"context"
 
+	"github.com/diyliv/store/config"
+	"github.com/diyliv/store/pkg/storage/timescaledb"
+)
+
+func main() {
+	ctx := context.Background()
+	cfg := config.ReadConfig("config", "yaml", "./config")
+	tsConn, err := timescaledb.ConnectToTimeScaleDb(ctx, cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := tsConn.Close(ctx); err != nil {
+			panic(err)
+		}
+	}()
 }
