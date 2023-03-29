03be3b506ebb85f86ddd985c54879985b9251da5
diyliv <onlogn081@gmail.com>
2023-03-29 18:20:49 +0300

    kafka conn

---
+++ pkg/kafka/kafka_conn.go
@@ -0,0 +1,18 @@
+package kafka
+
+import (
+	"context"
+
+	"github.com/segmentio/kafka-go"
+
+	"github.com/diyliv/store/config"
+)
+
+func NewKafkaConn(cfg *config.Config) (*kafka.Conn, error) {
+	return kafka.DialLeader(
+		context.Background(),
+		"tcp",
+		cfg.Kafka.Brokers[0],
+		cfg.Kafka.Topic,
+		cfg.Kafka.Partition)
+}
