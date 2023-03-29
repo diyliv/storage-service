f2a425b97504ed07023ee3e1cbac57c0e64d6afe
diyliv <onlogn081@gmail.com>
2023-03-29 18:03:10 +0300

    reading from config settings. pinging db

--- pkg/storage/timescaledb/timescaledb.go
+++ pkg/storage/timescaledb/timescaledb.go
@@ -1,5 +1,31 @@
 package timescaledb
 
-func ConnectToTimeScaleDb() {
-	connStr := "postgres://postgres:postgres@host:port/dbname?sslmode=require"
+import (
+	"context"
+	"fmt"
+
+	"github.com/jackc/pgx/v5"
+
+	"github.com/diyliv/store/config"
+)
+
+func ConnectToTimeScaleDb(ctx context.Context, cfg *config.Config) (*pgx.Conn, error) {
+	timeScaleInfo := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		cfg.TimeScaleDb.Login,
+		cfg.TimeScaleDb.Password,
+		cfg.TimeScaleDb.Host,
+		cfg.TimeScaleDb.Port,
+		cfg.TimeScaleDb.DB,
+		cfg.TimeScaleDb.SSLMode)
+
+	conn, err := pgx.Connect(ctx, timeScaleInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := conn.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	return conn, err
 }
