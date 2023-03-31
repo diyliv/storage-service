9d8bcc43cec68ac02d9ed80323e38bbff818698a
diyliv <onlogn081@gmail.com>
2023-03-31 18:11:33 +0300

    insert method

--- internal/repository/timescaledb.go
+++ internal/repository/timescaledb.go
@@ -2,22 +2,38 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/diyliv/store/internal/models"
 	"github.com/jackc/pgx/v5"
 	"go.uber.org/zap"
 )
 
 type repository struct {
-	logger        *zap.Logger
-	timescaleConn *pgx.Conn
+	logger    *zap.Logger
+	timescale *pgx.Conn
 }
 
-func NewRepository(logger *zap.Logger, timescaleConn *pgx.Conn) *repository {
+func NewRepository(logger *zap.Logger, timescale *pgx.Conn) *repository {
 	return &repository{
-		logger:        logger,
-		timescaleConn: timescaleConn,
+		logger:    logger,
+		timescale: timescale,
 	}
 }
 
-func (r *repository) Insert(ctx context.Context) {
+func (r *repository) Insert(ctx context.Context, metrics models.Response) error {
+	switch t := metrics.TagValue.(type) {
+	case float64:
+		if _, err := r.timescale.Exec(ctx, "INSERT INTO metrics(assetId, value, quality, tstamp) VALUES($1, $2, $3, $4)",
+			metrics.AssetId,
+			metrics.TagValue.(float64),
+			metrics.TagQuality,
+			metrics.ReadAt); err != nil {
+			r.logger.Error("Error while inserting data: " + err.Error())
+			return err
+		}
+	default:
+		fmt.Printf("%T\n", t)
+	}
+	return nil
 }
